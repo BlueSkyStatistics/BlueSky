973a50e45c519c177c0de968f60c94e3a7193b66
Aaron Rangel <aaron5025@hotmail.com>
2021-09-16 22:39:45 -0500

    Version change

--- BlueSky/R/BSkyVersion.r
+++ BlueSky/R/BSkyVersion.r
@@ -4,9 +4,9 @@
 ############################################################
 BSkyVersion<-function()
 {
-bskyver= "Version: 7.58"
-bskydate="Date: 2021-09-15"
-bskytime="08:37PM"
+bskyver= "Version: 7.59"
+bskydate="Date: 2021-09-16"
+bskytime="10:39PM"
 rver = R.Version()
 print("------ BlueSky R package version ------")
 print(bskyver) 
