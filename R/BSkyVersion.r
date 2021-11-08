32119cbcb320b1555a503e4373af3ea185351cc8
Aaron Rangel <aaron5025@hotmail.com>
2021-11-08 14:44:00 -0600

    Showing code that generated error/warning with appropriate heading for clarity

--- R/BSkyVersion.r
+++ R/BSkyVersion.r
@@ -14,8 +14,8 @@
 #' @examples BSkyVersion()
 BSkyVersion<-function()
 {
-bskyver= "Version: 7.80"
-bskydate="Date: 2021-10-22"
+bskyver= "Version: 7.81"
+bskydate="Date: 2021-11-08"
 bskytime="10:30AM"
 rver = R.Version()
 print("------ BlueSky R package version ------")
