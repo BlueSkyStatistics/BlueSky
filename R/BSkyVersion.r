b1c5a643333d7b7a0f90dca917b36b016793d6f5
AD <anildabralpro@gmail.com>
2021-12-16 11:49:47 +0530

    version incremented

--- R/BSkyVersion.r
+++ R/BSkyVersion.r
@@ -14,9 +14,9 @@
 #' @examples BSkyVersion()
 BSkyVersion<-function()
 {
-bskyver= "Version: 7.83"
-bskydate="Date: 2021-12-07"
-bskytime="12:36PM"
+bskyver= "Version: 7.84"
+bskydate="Date: 2021-12-16"
+bskytime="11:51AM"
 rver = R.Version()
 print("------ BlueSky R package version ------")
 print(bskyver) 
