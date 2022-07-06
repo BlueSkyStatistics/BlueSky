b40466fcb8dfb2280b2d022d4d0a738c761d513d
AD <anildabralpro@gmail.com>
2022-07-06 12:43:10 +0530

    error message modified

--- R/BSkySetWeight.r
+++ R/BSkySetWeight.r
@@ -28,8 +28,8 @@ BSkySetWeight <- function( weights, data, newdata)
 	}
 	else
 	{
-		errorMsg1 =paste("ERROR: There are missing values (NAs) in the weighted variable ", weights, "\n please corect this and rerun. See Data->Missing Values for options for handling missing values", weights, sep="", collapse="")
-		print(errorMsg1)
+		errorMsg1 =paste("ERROR: There are missing values (NAs) in the weighted variable ", weights, "\n please corect this and rerun. See Variables->Missing Values for options for handling missing values", sep="", collapse="")
+		cat(errorMsg1)
 		return(FALSE)
 	}
 }
