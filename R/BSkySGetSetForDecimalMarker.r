159ffa3b8ff28e85276eea5441c3ac302b4e489b
adpgithub <anildabralpro@gmail.com>
2024-07-10 20:21:03 +0530

    geter seter for decimal and display markers added

---
+++ R/BSkySGetSetForDecimalMarker.r
@@ -0,0 +1,55 @@
+# 07/08/24
+#Global Setting for decimal character marker for csv file read and cut/paste data
+BSkySetDecimalMarker <- function(decimalMarker = '') 
+{	
+	if(exists("uadatasets.sk"))
+	{
+		uadatasets.sk$BSkyDecimalMarker = decimalMarker[1]
+	}
+	
+	# Just a dummy return 
+	return(invisible(decimalMarker[1]))
+}
+
+# 07/08/24
+#Get the Global Setting for decimal character marker for csv file read and cut/paste data
+BSkyGetDecimalMarker <- function()
+{
+	decimalMarker = ''
+	
+	if(exists("uadatasets.sk") && exists("BSkyDecimalMarker", env=uadatasets.sk))
+	{
+		decimalMarker = uadatasets.sk$BSkyDecimalMarker[1]
+	}
+	
+	#return the actual decimal marker character that is currently set (from the triple dot setting options UI)
+	return(invisible(decimalMarker))
+}
+
+# 07/08/24
+#Global Setting for display character marker for csv file read and cut/paste data
+BSkySetDisplayMarker <- function(displayMarker = '') 
+{	
+	if(exists("uadatasets.sk"))
+	{
+		uadatasets.sk$BSkyDisplayMarker = displayMarker[1]
+	}
+	
+	# Just a dummy return 
+	return(invisible(displayMarker[1]))
+}
+
+# 07/08/24
+#Get the Global Setting for display character marker for csv file read and cut/paste data
+BSkyGetDisplayMarker <- function()
+{
+	displayMarker = ''
+	
+	if(exists("uadatasets.sk") && exists("BSkyDisplayMarker", env=uadatasets.sk))
+	{
+		decimalMarker = uadatasets.sk$BSkyDisplayMarker[1]
+	}
+	
+	#return the actual display marker character that is currently set (from the triple dot setting options UI)
+	return(invisible(displayMarker))
+}
