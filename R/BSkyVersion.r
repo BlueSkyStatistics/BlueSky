849726698f65bd609a80e079388aeef69fe6d217
adpgithub <anildabralpro@gmail.com>
2024-07-11 21:43:05 +0530

    version timestamp incremented

--- R/BSkyVersion.r
+++ R/BSkyVersion.r
@@ -16,8 +16,8 @@ BSkyVersion<-function(fulldetails=TRUE)
 {
 
 	bskyver= "Version: 8.97"
-	bskydate="Date: 2024-07-10"
-	bskytime="07:46PM"
+	bskydate="Date: 2024-07-11"
+	bskytime="09:09PM"
 	if(fulldetails)
 	{
 		rver = R.Version()
